601eb51f34b59d4db2c74be8370d7178556da950
mohamed <ryuugiri17@gmail.com>
2023-09-13 18:06:31 -0700

    wspace

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,13 +10,11 @@ int print_sign(int n)
 	{
 		_putchar(43);
 		return (1);
-	}
-	else if (n < 0)
+	}else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
-	}
-	else
+	}else
 	{
 		_putchar(48);
 		return (0);
